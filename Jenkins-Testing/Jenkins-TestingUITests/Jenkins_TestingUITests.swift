18091cebe3bb278abd65804ec6b1546136256baa
Amresh Kumar <amresh.amr@gmail.com>
2019-09-07 20:00:22 +0530

    exit code test

--- Jenkins-Testing/Jenkins-TestingUITests/Jenkins_TestingUITests.swift
+++ Jenkins-Testing/Jenkins-TestingUITests/Jenkins_TestingUITests.swift
@@ -26,8 +26,7 @@ class Jenkins_TestingUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        XCTFail()
+    func testExample(){
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
